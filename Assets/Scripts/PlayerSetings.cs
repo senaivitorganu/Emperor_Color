59efb6823f0b636de21a954389681bc97626e086
yzabelly001 <237255200+senaivitorganu@users.noreply.github.com>
2026-08-11 09:05:51 -0300

    correção do player

--- Assets/Scripts/PlayerSetings.cs
+++ Assets/Scripts/PlayerSetings.cs
@@ -1,5 +1,6 @@
 // create by: Vitor Gabriel
 // date: 05/08/2026 as 13:20
+// update: 11/08/2026 as 09:05
 
 using System.Collections;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ public class PlayerSetings : MonoBehaviour
 {
     [Header("Atributos do Player")]
     public float velocidade = 5f; // velocidade
+    public bool estaVivo = true; // se esta vivo
 
     [Header("Sprites do Player")]
     public PlayerAnimationController playerAnim; // coloca aonde esta script
@@ -56,34 +58,7 @@ public class PlayerSetings : MonoBehaviour
     }
 
     void Update()
-    { 
-        if (Input.GetAxisRaw("Horizontal") == -1) // caso usar A colocar script andando para esquerda
-        {
-            transform.position -= transform.right * (Time.deltaTime * velocidade);
-            direcaoAtual = Direcao.Esquerda;
-            playerAnim.PlayAnimation("ThePurpleKingWalkLeftAnimation");
-        }
-
-        if (Input.GetAxisRaw("Horizontal") == 1) // caso usar D colocar script andando para direita
-        {
-            transform.position += transform.right * (Time.deltaTime * velocidade);
-            direcaoAtual = Direcao.Direita;
-            playerAnim.PlayAnimation("ThePurpleKingWalkRightAnimation");
-        }
-
-        if (Input.GetAxisRaw("Vertical") == -1)
-        {
-            transform.position -= transform.up * (Time.deltaTime * velocidade); // caso use o S colocar o script andando para baixo
-            direcaoAtual = Direcao.Baixo;
-            playerAnim.PlayAnimation("ThePurpleKingWalkAnimation");
-        }
-
-        if (Input.GetAxisRaw("Vertical") == 1)
-        {
-            transform.position += transform.up * (Time.deltaTime * velocidade); // caso apertar W colocar a sprite de costas
-            direcaoAtual = Direcao.Cima;
-            playerAnim.PlayAnimation("ThePurpleKingWalkAnimationBack_");
-        }
+    {
 
         if (Input.GetMouseButtonDown(0) && !atacando) 
         {
@@ -92,6 +67,37 @@ public class PlayerSetings : MonoBehaviour
 
         if (!atacando) 
         {
+            //movimentar 
+            if (Input.GetAxisRaw("Horizontal") == -1) // caso usar A colocar script andando para esquerda
+            {
+                transform.position -= transform.right * (Time.deltaTime * velocidade);
+                direcaoAtual = Direcao.Esquerda;
+                playerAnim.PlayAnimation("ThePurpleKingWalkLeftAnimation");
+            }
+
+            if (Input.GetAxisRaw("Horizontal") == 1) // caso usar D colocar script andando para direita
+            {
+                transform.position += transform.right * (Time.deltaTime * velocidade);
+                direcaoAtual = Direcao.Direita;
+                playerAnim.PlayAnimation("ThePurpleKingWalkRightAnimation");
+            }
+
+            if (Input.GetAxisRaw("Vertical") == -1)
+            {
+                transform.position -= transform.up * (Time.deltaTime * velocidade); // caso use o S colocar o script andando para baixo
+                direcaoAtual = Direcao.Baixo;
+                playerAnim.PlayAnimation("ThePurpleKingWalkAnimation");
+            }
+
+            if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                transform.position += transform.up * (Time.deltaTime * velocidade); // caso apertar W colocar a sprite de costas
+                direcaoAtual = Direcao.Cima;
+                playerAnim.PlayAnimation("ThePurpleKingWalkAnimationBack_");
+            }
+
+
+            //idle
             if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
             {
                 switch (direcaoAtual)
